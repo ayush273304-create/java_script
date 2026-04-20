940f0881ccf4d30807f050ce761f55c7acab594b
ayush273304-create <ayush273304@gmail.com>
2026-04-20 18:27:28 +0000

    conversion of datatypes

---
+++ 01_basic/03.conversio_of_datatype.js
@@ -0,0 +1,28 @@
+let score = "44idsh"
+
+console.log(typeof score);
+console.log(typeof (score)); ///both are same 
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+///" 33"=> 33
+////"33abc"=>NaN
+// true=> 1 && false => 0
+
+
+let isLooedIn = 1
+let convertInBool =Boolean(isLooedIn);
+console.log(convertInBool);
+
+
+/// 1 => true  0=> false
+// "" => false
+/// name => true
+
+let someNumber = 33
+let change = String(someNumber)
+console.log(change)
+console.log(typeof change)
