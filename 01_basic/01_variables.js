74cf88d917a9fd0d18cab685daea4e2b2b9d9fc8
ayush273304-create <ayush273304@gmail.com>
2026-04-20 17:15:21 +0000

    container config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+ const account_ID = 2513628
+ let accountEmail = "ayushji287363@gmail.com"
+ var account_Password = "251310"
+ accountCity = " Uttar Pradesh"
+
+//  account_ID = 34    constant hai naa toh change nai ho skat hai
+ accountEmail = "MrAyush7soe.in"
+ account_Password =  "234342";
+ accountCity = "Delhi "
+//  console.log(account_ID);
+console.table([accountEmail , accountCity , account_Password])
