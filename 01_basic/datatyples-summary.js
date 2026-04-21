ccb1d8c541677ee5c44a6b4d6c4531258adf0a9b
ayush273304-create <ayush273304@gmail.com>
2026-04-21 05:34:45 +0000

    datatype summary

---
+++ 01_basic/datatyples-summary.js
@@ -0,0 +1,36 @@
+///Primitive 
+
+/// 7 types : String  , Number , Boolebn , null , Undefined , Symbol  , BigInt
+
+const score = 100
+const someNumber = 100.3
+
+const isLogged = false
+const outsideTemp = null
+let userId;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherID);
+
+// const bigNumber = 123456789876543456n
+
+
+
+//// Reference (Non Primimative)
+/// array , Object , Functions 
+
+const heros = ["shaktiman " , "naagraj", " doga"] ///array
+////object
+let myObj = {
+    name  : "ayush",
+    age : 19,
+}
+
+// function
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof BigInt);
